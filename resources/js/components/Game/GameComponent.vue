176ab8bb87899d90d682646b1eda43eb67060aca
Luis Angel <angel.rh.2108@gmail.com>
2022-10-07 18:43:37 -0500

    Pantalla de juego - Diseño

--- resources/js/components/Game/GameComponent.vue
+++ resources/js/components/Game/GameComponent.vue
@@ -1,15 +1,100 @@
 <template>
-<div class="container">
-    <p>jugar</p>
+<div class="container animate__animated animate__fadeInDown animate__faster">
+    <time-game />
+    <br>
+    <board-game />
+    <br>
+    <!-- Actions -->
+    <div class="text-center ms-3">
+        <span v-for="(a,i) in list_actions" :key="i" :class="'game-actions '+a.class" data-bs-toggle="tooltip" data-bs-placement="top" :title="a.tooltip">
+            <i :class="'game-action fa-solid '+a.icon"></i>
+        </span>
+    </div>
 </div>
 </template>
 
+<style>
+.game-actions {
+    cursor: pointer;
+    border: none;
+    border-radius: 10px;
+    padding: .5rem;
+    font-size: 1.5rem;
+    margin-right: 1rem;
+    display: inline-flex;
+}
+
+.game-action {
+    height: 1.5rem;
+    width: 1.5rem;
+    display: block;
+    color: white;
+}
+
+.game-actions_print {
+    background-color: var(--primary);
+}
+
+.game-actions_hint {
+    background-color: var(--yellow);
+}
+
+.game-actions_exit {
+    background-color: var(--red);
+}
+
+.game-actions_restart {
+    background-color: var(--green);
+}
+</style>
+
 <script>
+const BoardGame = r => require.ensure([], () => r(require("./Board/BoardGame.vue")), "game");
+const TimeComponent = r => require.ensure([], () => r(require("./TimeComponent.vue")), "game");
 export default
 {
+    data()
+    {
+        return {
+            list_actions: [],
+        }
+    },
+    components:
+    {
+        "board-game": BoardGame,
+        "time-game": TimeComponent
+    },
+    methods:
+    {
+        Init()
+        {
+
+            this.list_actions = [
+            {
+                class: "game-actions_print",
+                tooltip: "Imprimir",
+                icon: "fa-print",
+            },
+            {
+                class: "game-actions_hint",
+                tooltip: "Ayuda",
+                icon: "fa-lightbulb",
+            },
+            {
+                class: "game-actions_restart",
+                tooltip: "Reiniciar",
+                icon: "fa-rotate-right",
+            },
+            {
+                class: "game-actions_exit",
+                tooltip: "Salir",
+                icon: "fa-right-from-bracket",
+            }, ];
+        },
+    },
     mounted()
     {
-        console.log('Component mounted.')
+        this.Init();
     }
 }
 </script>
